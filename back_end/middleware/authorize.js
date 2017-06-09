b2abf6aa57f1974091dd1ff6c8ccc364dd3aa31b
Howie Wang <wanghowie@gmail.com>
2017-06-09 11:45:43 -0400

    sloved problem

--- back_end/middleware/authorize.js
+++ back_end/middleware/authorize.js
@@ -4,9 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (key) => {
 	return (req,res,next) => {
 		//check if there is token in the body, a url parameter, or the request headers
-		console.log(req.headers)
-    	let token = req.headers.authorization;
-		console.log(token);
+    	let token = req.headers.authorization;	
 		if (token) {
 			//try and decode token with the key that was used to encrypt it
             jwt.verify(token, key, function(err, decoded) {          
